c1d57717183e8508188c8284fc1660d23ecae967
Riddhi Sakpal <riddhisakpal43@gmail.com>
2021-08-26 17:24:23 +0530

    modified diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber=6, secondNumber=3 , sumOfTwoNumbers;
+
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    sumOfTwoNumbers = firstNumber - secondNumber;
+
+    // Prints sum 
+    cout << firstNumber << " - " << secondNumber << " = " << sumOfTwoNumbers;
+
+    return 0;
+}
